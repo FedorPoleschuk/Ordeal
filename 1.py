81a794c5e8500fa65ee9e8f5c6571c274e212906
Fedor Poleschuk <poleschuk.fa@phystech.edu>
2016-12-13 12:02:23 +0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,9 +1,10 @@
 x = int(input())
+
 i = 0
-while x > 16:
-    if x % 17 == 16:
+while x > 33:
+    if x % 36 == 33:
         i += 1
-    x = x // 17
-if x == 16:
+    x = x // 36
+if x == 33:
     i += 1
 print(i)
